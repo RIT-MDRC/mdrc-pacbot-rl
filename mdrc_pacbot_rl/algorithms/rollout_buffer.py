daee13f8ca53980c1fd60d5d6d3effac5f48e95e
Ben Giacalone <bensgiacalone@gmail.com>
2023-02-24 20:25:24 -0500

    WIP get PPO working

---
+++ mdrc_pacbot_rl/algorithms/rollout_buffer.py
@@ -0,0 +1,165 @@
+"""
+A rollout buffer for use with on-policy algorithms.
+Unlike a replay buffer, rollouts only store experience collected under a single policy.
+"""
+from typing import List, Tuple
+import torch
+from torch import nn
+
+
+class RolloutBuffer:
+    """
+    Stores transitions and generates mini batches from the latest policy.
+    Also computes advantage estimates.
+    """
+
+    def __init__(
+        self,
+        state_shape: torch.Size,
+        action_shape: torch.Size,
+        action_dtype: torch.dtype,
+        num_envs: int,
+        num_steps: int,
+        device: torch.device,
+    ):
+        k = torch.float
+        state_shape = [num_steps + 1, num_envs] + list(state_shape)
+        action_shape = [num_steps, num_envs] + list(action_shape)
+        self.num_envs = num_envs
+        self.num_steps = num_steps
+        self.next = 0
+        self.states = torch.zeros(
+            state_shape, dtype=k, device=device, requires_grad=False
+        )
+        self.actions = torch.zeros(
+            action_shape, dtype=action_dtype, device=device, requires_grad=False
+        )
+        self.rewards = torch.zeros(
+            [num_steps, num_envs], dtype=k, device=device, requires_grad=False
+        )
+        self.dones = torch.zeros(
+            [num_steps, num_envs], dtype=k, device=device, requires_grad=False
+        )
+        self.device = device
+
+    def insert_step(
+        self,
+        states: torch.Tensor,
+        actions: torch.Tensor,
+        rewards: List[float],
+        dones: List[bool],
+    ):
+        """
+        Inserts a transition from each environment into the buffer.
+        Make sure more data than steps aren't inserted.
+        Insert the state that was observed PRIOR to performing the action.
+        The final state returned will be inserted using `insert_final_step`.
+        """
+        with torch.no_grad():
+            self.states[self.next].copy_(states)
+            self.actions[self.next].copy_(actions)
+            self.rewards[self.next].copy_(
+                torch.tensor(rewards, dtype=torch.float, device=self.device)
+            )
+            self.dones[self.next].copy_(
+                torch.tensor(dones, dtype=torch.float, device=self.device)
+            )
+        self.next += 1
+
+    def insert_final_step(self, states: torch.Tensor):
+        """
+        Inserts the final observation observed.
+        """
+        with torch.no_grad():
+            self.states[self.next].copy_(states)
+
+    def samples(
+        self, batch_size: int, discount: float, lambda_: float, v_net: nn.Module
+    ) -> list[
+        Tuple[
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+        ]
+    ]:
+        """
+        Generates minibatches of experience, incorporating advantage estimates.
+        Returns previous states, states, actions, rewards, rewards to go, advantages, and dones.
+        """
+        with torch.no_grad():
+            tensor_opts = dict(dtype=torch.float, device=self.device)
+            rewards_to_go = torch.zeros([self.num_steps, self.num_envs], **tensor_opts)
+            advantages = torch.zeros([self.num_steps, self.num_envs], **tensor_opts)
+            step_rewards_to_go: torch.Tensor = v_net(
+                torch.unsqueeze(self.states[self.next], 0)
+            ).squeeze()
+
+            # Calculate advantage estimates and rewards to go
+            state_values = step_rewards_to_go.clone()
+            step_advantages = torch.zeros([self.num_envs], **tensor_opts)
+            for i in reversed(range(self.num_steps)):
+                prev_states = self.states[i]
+                rewards = self.rewards[i]
+                inv_dones = 1.0 - self.dones[i]
+                prev_state_values: torch.Tensor = v_net(
+                    prev_states.unsqueeze(0)
+                ).squeeze()
+                delta = (
+                    rewards + discount * inv_dones * state_values - prev_state_values
+                )
+                step_rewards_to_go = rewards + discount * step_rewards_to_go * inv_dones
+                state_values = prev_state_values
+                step_advantages = (
+                    delta + discount * lambda_ * inv_dones * step_advantages
+                )
+                advantages[i] = step_advantages
+                rewards_to_go[i] = step_rewards_to_go
+                step_advantages = (
+                    delta + discount * lambda_ * step_advantages * inv_dones
+                )
+                advantages[i] = step_advantages
+                state_values = prev_state_values
+
+            # Permute transitions to decorrelate them
+            exp_count = self.num_envs * self.num_steps
+            indices = torch.randperm(exp_count, dtype=torch.int, device=self.device)
+            rand_prev_states = self.states.flatten(0, 1).index_select(0, indices)
+            rand_actions = self.actions.flatten(0, 1).index_select(0, indices)
+            rand_rewards = self.rewards.flatten(0, 1).index_select(0, indices)
+            rand_rewards_to_go = rewards_to_go.flatten(0, 1).index_select(0, indices)
+            rand_advantages = advantages.flatten(0, 1).index_select(0, indices)
+            rand_dones = self.dones.flatten(0, 1).index_select(0, indices)
+            rand_states = self.states.flatten(0, 1).index_select(0, (indices + 1))
+            batch_count = exp_count // batch_size
+            batches = []
+            for i in range(batch_count):
+                start = i * batch_size
+                end = (i + 1) * batch_size
+                batches.append(
+                    (
+                        rand_prev_states[start:end].reshape(
+                            [batch_size] + list(self.states.shape)[2:]
+                        ),
+                        rand_states[start:end].reshape(
+                            [batch_size] + list(self.states.shape)[2:]
+                        ),
+                        rand_actions[start:end].reshape(
+                            [batch_size] + list(self.actions.shape)[2:]
+                        ),
+                        rand_rewards[start:end].reshape([batch_size, 1]),
+                        rand_rewards_to_go[start:end].reshape([batch_size, 1]),
+                        rand_advantages[start:end].reshape([batch_size, 1]),
+                        rand_dones[start:end].reshape([batch_size, 1]),
+                    )
+                )
+            return batches
+
+    def clear(self):
+        """
+        Clears the buffer.
+        """
+        self.next = 0
--- mdrc_pacbot_rl/experiments/test_ppo.py
+++ mdrc_pacbot_rl/experiments/test_ppo.py
@@ -1,58 +1,162 @@
 """
 Experiment for checking that PPO is working.
 """
+from functools import reduce
+from typing import Tuple
+import torch
 import torch.nn as nn
+from torch.distributions import Categorical
 from matplotlib import pyplot as plt
 from gymnasium.envs.classic_control.cartpole import CartPoleEnv
+from tqdm import tqdm
 
-class MyModel(TorchModelV2, nn.Module):
-    def __init__(self, *args, **kwargs):
-        TorchModelV2.__init__(self, *args, **kwargs)
+from mdrc_pacbot_rl.algorithms.rollout_buffer import RolloutBuffer
+
+# Constants (for now)
+train_steps = 128
+iterations = 50000 // train_steps
+train_iters = 2
+train_batch_size = 64
+discount = 1.0
+lambda_ = 0.95
+epsilon = 0.2
+device = torch.device("cpu")
+
+
+def copy_params(src: nn.Module, dest: nn.Module):
+    """
+    Copies params from one model to another.
+    """
+    with torch.no_grad():
+        for dest_, src_ in zip(dest.parameters(), src.parameters()):
+            dest_.data.copy_(src_.data)
+
+
+class ValueNet(nn.Module):
+    def __init__(self, obs_shape: torch.Size):
         nn.Module.__init__(self)
-        obs_space = args[0]
-        action_space = args[1]
-
-        # Action layers
-        self.a_layer1 = nn.Linear(obs_space.shape[0], 32)
-        self.a_layer2 = nn.Linear(32, 32)
-        self.a_layer3 = nn.Linear(32, self.num_outputs)
-        self.l_relu = nn.LeakyReLU(0.01)
-
-        # Value layers
-        self.v_layer1 = nn.Linear(obs_space.shape[0], 32)
+        flat_obs_dim = reduce(lambda e1, e2: e1 * e2, obs_shape, 1)
+        self.v_layer1 = nn.Linear(flat_obs_dim, 32)
         self.v_layer2 = nn.Linear(32, 32)
         self.v_layer3 = nn.Linear(32, 1)
+        self.l_relu = nn.LeakyReLU(0.01)
 
-    def forward(self, input_dict, state, seq_lens):
-        obs = input_dict["obs"]
-
-        # Value
-        x = self.v_layer1(obs)
+    def forward(self, input: torch.Tensor):
+        x = self.v_layer1(input.flatten(1))
         x = self.l_relu(x)
         x = self.v_layer2(x)
         x = self.l_relu(x)
         x = self.v_layer3(x)
-        self._value = x
+        return x
 
-        # Action
-        x = self.a_layer1(obs)
+
+class PolicyNet(nn.Module):
+    def __init__(self, obs_shape: torch.Size, action_count: int):
+        nn.Module.__init__(self)
+        flat_obs_dim = reduce(lambda e1, e2: e1 * e2, obs_shape, 1)
+        self.a_layer1 = nn.Linear(flat_obs_dim, 32)
+        self.a_layer2 = nn.Linear(32, 32)
+        self.a_layer3 = nn.Linear(32, action_count)
+        self.l_relu = nn.LeakyReLU(0.01)
+        self.logits = nn.LogSoftmax(1)
+
+    def forward(self, input: torch.Tensor):
+        x = self.a_layer1(input.flatten(1))
         x = self.l_relu(x)
         x = self.a_layer2(x)
         x = self.l_relu(x)
         x = self.a_layer3(x)
+        x = self.logits(x)
+        return x
 
-        return (x, state)
 
-    def value_function(self):
-        return self._value.squeeze(-1)
+env = CartPoleEnv()
+reward_totals = []
 
-algo = config.build()
+# Init PPO
+obs_space = env.observation_space
+act_space = env.action_space
+v_net = ValueNet(obs_space.shape)
+p_net = PolicyNet(obs_space.shape, act_space.n)
+p_net_old = PolicyNet(obs_space.shape, act_space.n)
+p_net_old.eval()
+v_opt = torch.optim.Adam(v_net.parameters(), lr=0.001)
+p_opt = torch.optim.Adam(p_net.parameters(), lr=0.003)
+buffer = RolloutBuffer(
+    obs_space.shape, torch.Size((act_space.n,)), torch.int, 1, train_steps, device
+)
 
-reward_means = []
-for _ in range(10):
-    results = algo.train()
-    reward_mean = results["episode_reward_mean"]
-    reward_means.append(reward_mean)
+obs = torch.Tensor(env.reset()[0])
+done = False
+for _ in tqdm(range(iterations), position=0):
+    # Collect experience
+    with torch.no_grad():
+        for _ in tqdm(range(train_steps), position=1):
+            action = (
+                Categorical(logits=p_net(obs.unsqueeze(0)).squeeze()).sample().item()
+            )
+            obs_, reward, done, _, _ = env.step(action)
+            buffer.insert_step(
+                obs.unsqueeze(0), torch.Tensor([action]), [reward], [done]
+            )
+            obs = torch.Tensor(obs_)
+            if done:
+                obs = torch.Tensor(env.reset()[0])
+                done = False
+        buffer.insert_final_step(obs.unsqueeze(0))
 
-plt.plot(reward_means)
+    # Train
+    p_net.train()
+    v_net.train()
+    copy_params(p_net, p_net_old)
+
+    for _ in range(train_iters):
+        batches = buffer.samples(train_batch_size, discount, lambda_, v_net)
+        for prev_states, _, actions, _, rewards_to_go, advantages, _ in batches:
+            # Train policy network
+            old_log_probs = p_net_old(prev_states)
+            p_opt.zero_grad()
+            new_log_probs = p_net(prev_states)
+            term1: torch.Tensor = (new_log_probs - old_log_probs).exp() * advantages
+            term2: torch.Tensor = (1.0 + epsilon * advantages.sign()) * advantages
+            p_loss = -(term1.minimum(term2).mean())
+            p_loss.backward()
+            p_opt.step()
+
+            # Train value network
+            v_opt.zero_grad()
+            diff: torch.Tensor = v_net(prev_states) - rewards_to_go.unsqueeze(1)
+            v_loss = (diff * diff).mean()
+            v_loss.backward()
+            v_opt.step()
+
+    p_net.eval()
+    v_net.eval()
+    buffer.clear()
+
+    # Evaluate
+    obs = torch.Tensor(env.reset()[0])
+    done = False
+    with torch.no_grad():
+        reward_total = 0
+        for _ in range(8):
+            for _ in range(16):
+                action = (
+                    Categorical(logits=p_net(obs.unsqueeze(0)).squeeze())
+                    .sample()
+                    .item()
+                )
+                obs_, reward, done, _, _ = env.step(action)
+                obs = torch.Tensor(obs_)
+                reward_total += reward
+                if done:
+                    obs = torch.Tensor(env.reset()[0])
+                    done = False
+                    break
+        reward_totals.append(reward_total)
+
+    obs = torch.Tensor(env.reset()[0])
+    done = False
+
+plt.plot(reward_totals)
 plt.show()
