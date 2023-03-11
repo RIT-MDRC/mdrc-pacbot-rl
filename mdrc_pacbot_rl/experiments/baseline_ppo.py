a64b90120f21bfd85bcdbe34acfb18b6f7fd19ff
Ben Giacalone <24901846+Boxxfish@users.noreply.github.com>
2023-03-11 02:34:08 -0500

    WIP: Fix baseline PPO experiment (#3)

--- mdrc_pacbot_rl/experiments/baseline_ppo.py
+++ mdrc_pacbot_rl/experiments/baseline_ppo.py
@@ -1,6 +1,11 @@
 """
 Baseline for PPO on Pacman gym.
+
+CLI Args:
+    --eval: Run the last saved policy in the test environment, with visualization.
+    --resume: Resume training from the last saved policy.
 """
+import sys
 from typing import Any
 
 import torch
@@ -13,69 +18,99 @@ from tqdm import tqdm
 
 from mdrc_pacbot_rl.algorithms.rollout_buffer import RolloutBuffer
 from mdrc_pacbot_rl.pacman.gym import PacmanGym
-from mdrc_pacbot_rl.utils import copy_params, init_orthogonal
+from mdrc_pacbot_rl.utils import copy_params, get_img_size, init_orthogonal
 
 _: Any
 
 # Hyperparameters
 num_envs = 128
-train_steps = 500
-iterations = 1000
+train_steps = 100
+iterations = 10
 train_iters = 2
 train_batch_size = 512
-discount = 0.98
+discount = 0.95
 lambda_ = 0.95
 epsilon = 0.2
 max_eval_steps = 100
+v_lr = 0.001
+p_lr = 0.0001
 device = torch.device("cpu")
 
 
 class ValueNet(nn.Module):
     def __init__(self, obs_shape: torch.Size):
         nn.Module.__init__(self)
+        w, h = obs_shape[1:]
         self.cnn1 = nn.Conv2d(obs_shape[0], 8, 3)
-        self.cnn2 = nn.Conv2d(8, 16, 3)
-        self.v_layer1 = nn.Linear(1736, 256)
-        self.v_layer2 = nn.Linear(256, 256)
-        self.v_layer3 = nn.Linear(256, 1)
+        h, w = get_img_size(h, w, self.cnn1)
+        self.cnn2 = nn.Conv2d(8, 12, 3)
+        h, w = get_img_size(h, w, self.cnn2)
+        flat_dim = h * w * self.cnn2.out_channels
+        self.v_layer1 = nn.Linear(flat_dim, 512)
+        self.v_layer2 = nn.Linear(512, 1)
         self.relu = nn.ReLU()
         init_orthogonal(self)
 
     def forward(self, input: torch.Tensor):
-        x = self.v_layer1(input.flatten(1))
+        x = self.cnn1(input)
+        x = self.relu(x)
+        x = self.cnn2(x)
+        x = x.flatten(1)
+        x = self.relu(x)
+        x = self.v_layer1(x)
         x = self.relu(x)
         x = self.v_layer2(x)
-        x = self.relu(x)
-        x = self.v_layer3(x)
         return x
 
 
 class PolicyNet(nn.Module):
     def __init__(self, obs_shape: torch.Size, action_count: int):
         nn.Module.__init__(self)
+        w, h = obs_shape[1:]
         self.cnn1 = nn.Conv2d(obs_shape[0], 8, 3)
-        self.cnn2 = nn.Conv2d(8, 16, 3)
-        self.a_layer1 = nn.Linear(1736, 256)
-        self.a_layer2 = nn.Linear(256, 256)
-        self.a_layer3 = nn.Linear(256, action_count)
+        h, w = get_img_size(h, w, self.cnn1)
+        self.cnn2 = nn.Conv2d(8, 12, 3)
+        h, w = get_img_size(h, w, self.cnn2)
+        flat_dim = h * w * self.cnn2.out_channels
+        self.a_layer1 = nn.Linear(flat_dim, 512)
+        self.a_layer2 = nn.Linear(512, action_count)
         self.relu = nn.ReLU()
         self.logits = nn.LogSoftmax(1)
         init_orthogonal(self)
 
     def forward(self, input: torch.Tensor):
-        x = self.a_layer1(input.flatten(1))
+        x = self.cnn1(input)
+        x = self.relu(x)
+        x = self.cnn2(x)
+        x = x.flatten(1)
+        x = self.relu(x)
+        x = self.a_layer1(x)
         x = self.relu(x)
         x = self.a_layer2(x)
-        x = self.relu(x)
-        x = self.a_layer3(x)
         x = self.logits(x)
         return x
 
 
-env = SyncVectorEnv([lambda: PacmanGym()] * num_envs)
-test_env = PacmanGym()
+env = SyncVectorEnv([lambda: PacmanGym(random_start=True)] * num_envs)
+test_env = PacmanGym(random_start=True)
 reward_totals = []
 entropies = []
+
+# If evaluating, just run the eval env
+if len(sys.argv) >= 2 and sys.argv[1] == "--eval":
+    test_env = PacmanGym(render_mode="human")
+    p_net = torch.load("temp/PNet.pt")
+    with torch.no_grad():
+        obs = torch.Tensor(test_env.reset()[0])
+        for _ in range(max_eval_steps):
+            distr = Categorical(logits=p_net(obs.unsqueeze(0)).squeeze())
+            action = distr.sample().item()
+            obs_, reward, done, _, _ = test_env.step(action)
+            obs = torch.Tensor(obs_)
+            print(reward)
+            if done:
+                break
+    quit()
 
 # Init PPO
 obs_shape = env.envs[0].observation_space.shape
@@ -85,12 +120,17 @@ obs_size = torch.Size(obs_shape)
 act_space = env.envs[0].action_space
 if not isinstance(act_space, Discrete):
     raise RuntimeError("Action space was not discrete")
-v_net = ValueNet(torch.Size(obs_shape))
-p_net = PolicyNet(obs_size, int(act_space.n))
-p_net_old = PolicyNet(obs_size, int(act_space.n))
+if len(sys.argv) >= 2 and sys.argv[1] == "--resume":
+    v_net = torch.load("temp/VNet.pt")
+    p_net = torch.load("temp/PNet.pt")
+    p_net_old = torch.load("temp/PNet.pt")
+else:
+    v_net = ValueNet(torch.Size(obs_shape))
+    p_net = PolicyNet(obs_size, int(act_space.n))
+    p_net_old = PolicyNet(obs_size, int(act_space.n))
 p_net_old.eval()
-v_opt = torch.optim.Adam(v_net.parameters(), lr=0.01)
-p_opt = torch.optim.Adam(p_net.parameters(), lr=0.0001)
+v_opt = torch.optim.Adam(v_net.parameters(), lr=v_lr)
+p_opt = torch.optim.Adam(p_net.parameters(), lr=p_lr)
 buffer = RolloutBuffer(
     obs_size,
     torch.Size((1,)),
@@ -101,7 +141,6 @@ buffer = RolloutBuffer(
 )
 
 obs = torch.Tensor(env.reset()[0])
-done = False
 for _ in tqdm(range(iterations), position=0):
     # Collect experience
     with torch.no_grad():
@@ -112,9 +151,6 @@ for _ in tqdm(range(iterations), position=0):
                 obs, torch.from_numpy(actions).unsqueeze(-1), rewards, dones
             )
             obs = torch.from_numpy(obs_)
-            if done:
-                obs = torch.Tensor(env.reset()[0])
-                done = False
         buffer.insert_final_step(obs)
 
     # Train
@@ -122,7 +158,7 @@ for _ in tqdm(range(iterations), position=0):
     v_net.train()
     copy_params(p_net, p_net_old)
 
-    for _ in range(train_iters):
+    for _ in tqdm(range(train_iters), position=1):
         batches = buffer.samples(train_batch_size, discount, lambda_, v_net)
         for prev_states, _, actions, _, rewards_to_go, advantages, _ in batches:
             # Train policy network
@@ -154,37 +190,34 @@ for _ in tqdm(range(iterations), position=0):
     buffer.clear()
 
     # Evaluate
-    obs = torch.Tensor(test_env.reset()[0])
-    done = False
+    eval_done = False
     with torch.no_grad():
         # Visualize
         reward_total = 0
         entropy_total = 0.0
-        obs = torch.Tensor(test_env.reset()[0])
-        eval_steps = 8
+        eval_obs = torch.Tensor(test_env.reset()[0])
+        eval_steps = 4
         for _ in range(eval_steps):
             avg_entropy = 0.0
             steps_taken = 0
             for _ in range(max_eval_steps):
-                distr = Categorical(logits=p_net(obs.unsqueeze(0)).squeeze())
+                distr = Categorical(logits=p_net(eval_obs.unsqueeze(0)).squeeze())
                 action = distr.sample().item()
-                obs_, reward, done, _, _ = test_env.step(action)
-                obs = torch.Tensor(obs_)
+                obs_, reward, eval_done, _, _ = test_env.step(action)
+                eval_obs = torch.Tensor(obs_)
                 steps_taken += 1
-                if done:
-                    obs = torch.Tensor(test_env.reset()[0])
-                    break
                 reward_total += reward
                 avg_entropy += distr.entropy()
+                if eval_done:
+                    eval_obs = torch.Tensor(test_env.reset()[0])
+                    break
             avg_entropy /= steps_taken
             entropy_total += avg_entropy
         reward_totals.append(reward_total / eval_steps)
         entropies.append(entropy_total / eval_steps)
-    obs = torch.Tensor(env.reset()[0])
-    done = False
 
-torch.save(v_net, "VNet.pt")
-torch.save(p_net, "PNet.pt")
+torch.save(v_net, "temp/VNet.pt")
+torch.save(p_net, "temp/PNet.pt")
 
 figure, axis = plt.subplots(2, 1)
 axis[0].plot(reward_totals)
