ac41530238ab377d4c144783894c27b626ddb7ad
Ben Giacalone <bensgiacalone@gmail.com>
2023-03-11 14:47:40 -0500

    Move random actions baseline to wandb

--- mdrc_pacbot_rl/experiments/random_actions.py
+++ mdrc_pacbot_rl/experiments/random_actions.py
@@ -4,29 +4,35 @@ Experiment where the agent performs random actions.
 import torch.nn as nn
 from matplotlib import pyplot as plt  # type: ignore
 from tqdm import tqdm
-
+import wandb
 from mdrc_pacbot_rl.pacman.gym import PacmanGym
 
-# Hyperparameters
-train_steps = 100
+max_eval_steps = 300
+eval_steps = 8
+iterations = 300
 
-env = PacmanGym(render_mode="human")
+wandb.init(
+    project="pacbot",
+    entity="mdrc-pacbot",
+    config={"experiment": "random actions baseline"},
+)
+
+env = PacmanGym()
 action_space = env.action_space
 obs, _ = env.reset()
 done = False
-reward_means = []
-for _ in tqdm(range(100), position=0):
-    reward_mean = []
-    for _ in tqdm(range(train_steps), position=1):
-        action = action_space.sample()
-        obs, reward, done, _, _ = env.step(action)
-        if done:
-            obs = env.reset()
-            done = False
-        reward_mean.append(reward)
-    obs = env.reset()
-    done = False
-    reward_means.append(sum(reward_mean) / train_steps)
+for _ in tqdm(range(iterations)):
+    total_reward = 0.0
+    for _ in range(eval_steps):
+        for _ in range(max_eval_steps):
+            action = action_space.sample()
+            obs, reward, done, _, _ = env.step(action)
+            total_reward += reward
+            if done:
+                break
+        obs = env.reset()
+        done = False
+    avg_eval_episode_reward = total_reward / eval_steps
+    wandb.log({"avg_eval_episode_reward": avg_eval_episode_reward})
 
-plt.plot(reward_means)
-plt.show()
+wandb.finish()
