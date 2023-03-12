70a806d71d12760c8dde689667486509aed28af9
Ben Giacalone <bensgiacalone@gmail.com>
2023-03-11 20:02:51 -0500

    Fix imports

--- mdrc_pacbot_rl/experiments/random_actions.py
+++ mdrc_pacbot_rl/experiments/random_actions.py
@@ -2,10 +2,10 @@
 Experiment where the agent performs random actions.
 """
 import torch.nn as nn
+import wandb
 from matplotlib import pyplot as plt  # type: ignore
 from tqdm import tqdm
 
-import wandb
 from mdrc_pacbot_rl.pacman.gym import PacmanGym
 
 max_eval_steps = 300
